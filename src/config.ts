f86baf867585f277f62f8c618324624edf9f60d9
Khushang Dave <khushangdave@gmail.com>
2026-05-01 15:03:39 +0530

    Fix render.yaml syntax and config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+const API_URL = VITE_API_URL ? `${VITE_API_URL}/api` : 'http://localhost:5001/api';
+
 export default API_URL;
